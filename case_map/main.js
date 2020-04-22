5ce6e8568fe5345b76391064457d6876018f9395
yuchen <r82780736@gmail.com>
2020-04-22 16:32:44 +0800

    add main.js

--- case_map/main.js
+++ case_map/main.js
@@ -35,9 +35,16 @@ var appView = new ol.View({
   center: ol.proj.fromLonLat([120.221115, 22.964407]),
   zoom: 14
 });
+var dataLayer= new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: '../data.json',
+    format: new ol.format.GeoJSON()
+  })
+});
+
 
 var map = new ol.Map({
-  layers: [baseLayer],
+  layers: [baseLayer,dataLayer],
   target: 'map',
   view: appView
 });
